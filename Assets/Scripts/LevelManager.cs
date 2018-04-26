6d65bcc9b4ef109f4ce3756a0df2011681bfea10
nuclearMonkeys <33884355+nuclearMonkeys@users.noreply.github.com>
2018-04-26 14:52:54 -0700

    Progress made from BenNBranch copied to lynBranch

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public Checkpoint currentCheckpoint;
+    public GameObject player;
+
+	void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RespawnPlayer()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("Player Respawn");
+        //player.transform.position = currentCheckpoint.transform.position;
+    }
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    public int health;
+    public int armor;
+
+    void Start()
+    {
+        health = 100;
+        armor = 20;
+    }
+}
