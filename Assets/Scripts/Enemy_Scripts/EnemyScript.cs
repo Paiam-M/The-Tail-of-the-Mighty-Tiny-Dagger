e1a6275a03499da71134cf50430024c0fd131ef8
Darren <dartrenbbx@gmail.com>
2018-04-28 17:47:00 -0700

    added sprite for enemy and a "dagger"

--- Assets/Scripts/Enemy_Scripts/EnemyScript.cs
+++ Assets/Scripts/Enemy_Scripts/EnemyScript.cs
@@ -21,6 +21,8 @@ public class EnemyScript : MonoBehaviour {
     public float armor = 0f;
     public bool isEnemy = true;
 
+    private SpriteRenderer sRend;
+
 
     // Use this for initialization
     void Start ()
@@ -28,6 +30,7 @@ public class EnemyScript : MonoBehaviour {
         target = null;
         moveSpot.position = new Vector2(Random.Range(minX, maxX), transform.position.y);
         waitTime = startWaitTime;
+        sRend = GetComponent<SpriteRenderer>();
     }
 	
 	// Update is called once per frame
@@ -39,8 +42,13 @@ public class EnemyScript : MonoBehaviour {
         else
         {
             transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, transform.position.y), speed * Time.deltaTime);
+            if (isMovingRight(transform.position, target.position))
+                sRend.flipX = true;
+            else
+                sRend.flipX = false;
             Debug.Log("FOLLOWING PLAYER");
         }
+        
         if(transform.position.y < -10)
         {
             StartCoroutine(Death());
@@ -62,6 +70,10 @@ public class EnemyScript : MonoBehaviour {
     private void patrol()
     {
         transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
+        if (isMovingRight(transform.position, moveSpot.position))
+            sRend.flipX = true;
+        else
+            sRend.flipX = false;
         if (Mathf.Abs(transform.position.x - moveSpot.position.x) < 0.2f)
         {
             if (waitTime <= 0f)
@@ -74,6 +86,14 @@ public class EnemyScript : MonoBehaviour {
                 waitTime -= Time.deltaTime;
             }
         }
+    }
+
+    private bool isMovingRight(Vector2 pos, Vector2 newPos)
+    {
+        if ((pos.x - newPos.x) >= 0)
+            return false;
+        else
+            return true;
     }
 
     //if player has entered enemy range, set target to player
