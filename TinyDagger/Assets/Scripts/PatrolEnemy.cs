24024e522d987de9f45d474dc90b1cc4787dcc6d
Darren <dartrenbbx@gmail.com>
2018-05-07 22:53:24 -0700

    Added 2 moar enemy
    gotta implement damage and stuff

---
+++ TinyDagger/Assets/Scripts/PatrolEnemy.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//patrol enemy only patrols DOES NOT FOLLOW PLAYER
+public class PatrolEnemy : MonoBehaviour {
+
+    public float speed;
+    public float startWaitTime;
+    public Transform moveSpot;
+
+    private Transform target;
+    private float waitTime;
+    public float minX;
+    public float maxX;
+
+    private SpriteRenderer sRend;
+
+    public float health = 10f;
+    public float armor = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = null;
+        waitTime = startWaitTime;
+        moveSpot.position = new Vector2(Random.Range(minX, maxX), transform.position.y);
+        sRend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        patrol();
+        /*
+        //if we see player, then chase, else patrol
+        if (target == null)
+            patrol();
+        
+        else
+        {
+            
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, transform.position.y), speed * Time.deltaTime);
+            if (transform.position.x < target.position.x)
+                sRend.flipX = true;
+            else
+                sRend.flipX = false;
+            Debug.Log("FOLLOWING PLAYER");
+            
+        }*/
+
+    }
+
+    public void takeDamage(float dmg)
+    {
+        health -= dmg;
+        if (health <= 0)
+        {
+            //die die die
+            //maybe make a death animation later on
+            Destroy(gameObject);
+        }
+    }
+
+    //patrol around the platform. stop at points on the floor, and wait some time before moving again
+    private void patrol()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
+        if (transform.position.x < moveSpot.position.x)
+            sRend.flipX = true;
+        else
+            sRend.flipX = false;
+        if (Vector2.Distance(transform.position, moveSpot.position) < 0.2f)
+        {
+            if (waitTime <= 0f)
+            {
+                moveSpot.position = new Vector2(Random.Range(minX, maxX), transform.position.y);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    //if player has entered enemy range, set target to player
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            //player takes damage
+            //target = collider.transform;
+        }
+    }
+
+}
---
+++ TinyDagger/Assets/Scripts/StationaryEnemy.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationaryEnemy : MonoBehaviour {
+
+    private Transform target;
+
+    private SpriteRenderer sRend;
+    public float health = 10f;
+    public float armor = 0f;
+    public GameObject projectile;
+
+    // Use this for initialization
+    void Start ()
+    {
+        target = null;
+        sRend = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (target != null)
+        {
+            if (transform.position.x < target.position.x)
+                sRend.flipX = true;
+            else
+                sRend.flipX = false;
+            //check if the player is touching the enemy
+            if (Vector2.Distance(transform.position, target.position) < 2f)
+            {
+                //player takes damage
+            }
+        }
+	}
+
+    public void takeDamage(float dmg)
+    {
+        health -= dmg;
+        if (health <= 0)
+        {
+            //just die
+            Destroy(gameObject);
+        }
+    }
+
+    private void Fire()
+    {
+        Instantiate(projectile, transform.position, Quaternion.identity);
+    }
+
+    //if player has entered enemy range, set target to player
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            target = collider.transform;
+        }
+    }
+
+    //if player has exited, set target to null
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            target = null;
+        }
+    }
+
+}
