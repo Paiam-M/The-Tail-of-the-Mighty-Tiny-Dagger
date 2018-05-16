4ed1836eabbac91b1578575b5afead0e99163cde
nuclearMonkeys <lyndond@uci.edu>
2018-05-16 13:23:54 -0700

    Moved enemy stuff to BenG

---
+++ Assets/Scripts/Enemy_Scripts/FireBall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour {
+
+    public float speed;
+    public float damage = 5f;
+    private Transform player;
+    private Vector2 target;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(player.position.x, player.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, target) < 0.2f)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            //add player takes damage method
+            Destroy(gameObject);
+        }
+    }
+}
