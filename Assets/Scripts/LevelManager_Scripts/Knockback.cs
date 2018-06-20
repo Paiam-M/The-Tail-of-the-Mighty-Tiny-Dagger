b556075178d333b3e71c9a64b38b78c2d6aac4ed
nuclearMonkeys <lyndond@uci.edu>
2018-06-20 14:11:19 -0700

    Added Bouncepad
    - Added a bouncepad in my scene. It uses a knockback script which can be used for the player as well
    - Altered the moving platform so that when the player lands, the character doesn't stay exactly in the middle of the platform

---
+++ Assets/Scripts/LevelManager_Scripts/Knockback.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knockback : MonoBehaviour {
+
+    public GameObject player;
+    public float power;
+
+    private int time;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (time > 0)
+        {
+            Debug.Log("Hit");
+            player.GetComponent<Rigidbody2D>().AddForce(Vector2.up * power);
+            time--;
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            time += 5;
+        }
+    }
+}
---
+++ Assets/Scripts/LevelManager_Scripts/MovingPlatform.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    public Vector2   currentPoint;
+    public Vector2[] pointArray;
+    public int index;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 0.05f;
+        index = 0;
+        currentPoint = pointArray[index];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector2.MoveTowards(transform.position, currentPoint, speed);
+        if (transform.position.x == currentPoint.x && transform.position.y == currentPoint.y)
+        {
+            index++;
+            if (index == pointArray.Length)
+                index = 0;
+            currentPoint = pointArray[index];
+        }
+	}
+    /*
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.transform.position = new Vector2(transform.position.x, (transform.position.y / 4));
+        }
+    }*/
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        collision.transform.position = new Vector2( (transform.position.x + collision.transform.position.x)/2 , (transform.position.y / 3.08f ));
+
+        //collision.transform.position = new Vector2(transform.position.x, (transform.position.y / 6) );
+    }
+}
